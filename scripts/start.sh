364d49a269021e871bd3f6784f8329bfbd989b23
jhegge <johnathan.hegge@nimbix.net>
2020-01-27 17:40:15 -0600

    split into OpenGL vs Mesa

--- scripts/start.sh
+++ scripts/start.sh
@@ -2,4 +2,8 @@
 
 cd /data || true
 
-exec /usr/local/bin/nimbix_desktop /usr/local/paraview/bin/paraview
+if grep --quiet VGL_DISPLAY /etc/JARVICE/vglinfo.sh; then
+  exec /usr/local/bin/nimbix_desktop /usr/local/paraview/bin/paraview
+else
+  exec /usr/local/bin/nimbix_desktop /usr/local/paraview/bin/paraview-mesa paraview
+fi
